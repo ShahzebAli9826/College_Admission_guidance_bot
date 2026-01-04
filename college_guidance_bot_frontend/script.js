fdacc8a2972c2eb4614cd51f3da703b93511ce36
ShahzebAli9826 <shahzebali_cse_25a05051@dtu.ac.in>
2026-01-04 22:19:41 +0530

    added backend url

--- college_guidance_bot_frontend/script.js
+++ college_guidance_bot_frontend/script.js
@@ -36,7 +36,8 @@ async function sendMessage() {
     chatBox.scrollTop = chatBox.scrollHeight;
 
     try {
-        const res = await fetch("http://127.0.0.1:8000/chat", {
+        const res = await fetch("https://college-admission-backend.onrender.com/chat", {
+
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
